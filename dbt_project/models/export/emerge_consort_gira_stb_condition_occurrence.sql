b8a66f11bfe335a3d71caf7ec3c9fbc12c6aa52b
a-l-holmes <ann.142.holmes@gmail.com>
2026-03-06 16:33:43 +0000

    :wrench: working with new intermediate tables

--- dbt_project/models/export/emerge_consort_gira_stb_condition_occurrence.sql
+++ dbt_project/models/export/emerge_consort_gira_stb_condition_occurrence.sql
@@ -3,8 +3,8 @@
     select
     null::integer as "condition_occurrence_id",
     emerge_id::integer as "person_id",
-    concept_id::integer as "condition_concept_id",
-    null::text as "condition_start_date", -- required but unknown in data
+    s_condition_concept_id::integer as "condition_concept_id",
+    date_add(birth_date, INTERVAL (age_at_event) YEAR)::date as "condition_start_date", -- required but unknown in data
     null::timestamp as "condition_start_datetime",
     null::text as "condition_end_date",
     null::timestamp as "condition_end_datetime",
@@ -14,35 +14,19 @@
     null::integer as "provider_id",
     null::integer as "visit_occurrence_id",
     null::integer as "visit_detail_id",
-    concept_code::text as "condition_source_value",
-    null::integer as "condition_source_concept_id",
+    icd_code::text as "condition_source_value", -- Either the code from the source or the code joined to the id given in the source
+    icd_id::text as "condition_source_concept_id", -- Either the id from the source or the id that joined to the code
     null::text as "condition_status_source_value",
     row_id::integer as "x_row_id",
     encounter_id::integer as "x_encounter_id",
     gira_ror::text as "x_gira_ror"
-    from {{ ref('emerge_consort_gira_int_icd_observations') }} --place holder icd conditions
-    
-    union all
-    
-    select
-    null::integer as "condition_occurrence_id",
-    emerge_id::integer as "person_id",
-    concept_id::integer as "condition_concept_id",
-    null::text as "condition_start_date", -- required but unknown in data
-    null::timestamp as "condition_start_datetime",
-    null::text as "condition_end_date",
-    null::timestamp as "condition_end_datetime",
-    null::integer as "condition_type_concept_id",
-    null::integer as "condition_status_concept_id",
-    null::text as "stop_reason",
-    null::integer as "provider_id",
-    null::integer as "visit_occurrence_id",
-    null::integer as "visit_detail_id",
-    concept_code::text as "condition_source_value",
-    cpt_code::integer as "condition_source_concept_id",
-    null::text as "condition_status_source_value",
-    row_id::integer as "x_row_id",
-    encounter_id::integer as "x_encounter_id",
-    gira_ror::text as "x_gira_ror"
-    from {{ ref('emerge_consort_gira_int_cpt_observations') }} --place holder cpt conditions
-    
+    -- from {{ ref('emerge_consort_gira_int_icd_observations') }} --place holder icd conditions
+    from {{ ref('emerge_consort_gira_int_icd_conditions') }} as meas
+    left join (select
+        distinct emerge_id,
+        case when year_of_birth is not null
+             then make_date(CAST(year_of_birth as integer), 6, 15)
+             else make_date(1970, 6, 15)
+        end as birth_date--TODO What to do when year_of_birth is null?
+        from {{ ref('emerge_consort_gira_src_emerge_person_ex_release_20260123') }} ) as person
+    using (emerge_id)
