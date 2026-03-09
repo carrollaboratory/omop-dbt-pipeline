e308b66850bccb38f49c7d2437196cc4f884d351
brendagutman <brenda.gutman@gmail.com>
2026-03-09 16:36:27 +0000

    Move nones and add last two caresites

---
+++ dbt_project/models/emerge/consort_gira/intermediate/analysis/emerge_consort_gira_int_bmi_none.sql
@@ -0,0 +1,36 @@
+{{ config(materialized='table') }}
+-- Columns prefixed with 's_' represent the confirmed Standard value for the column. If the field is NULL no Standard could be found.
+        
+SELECT
+    emerge_id,
+    age_at_event,
+    measurement_concept_id,
+    measurement_concept_name,
+    mci.s_concept_id as "s_measurement_concept_id", 
+    mci.s_concept_code as "s_measurement_concept_code",
+    value_as_number,
+    unit_concept_id,
+    unit_concept_name,
+    uci.s_concept_id as "s_unit_concept_id",
+    uci.s_concept_code as "s_unit_concept_code",
+    bmi_z_score,
+    row_id,
+    encounter_id,
+    gira_ror,
+    src_index,
+FROM {{ ref('emerge_consort_gira_src_emerge_bmi_ex_release_20260128') }} src
+JOIN (SELECT -- JOIN used to drop rows that are not domain 'Measurement'
+      s_concept_id, s_concept_code, src_concept_id
+      FROM {{ ref('emerge_consort_gira_lookup_standards') }} 
+      WHERE src_table = 'BMI'
+      AND (domain_id is null
+          or domain_id is not in ('Measurement'))
+      ) AS mci
+    ON src.measurement_concept_id = mci.src_concept_id
+LEFT JOIN (SELECT
+      s_concept_id, s_concept_code, src_concept_id
+      FROM {{ ref('emerge_consort_gira_lookup_standards') }} 
+      WHERE src_table = 'BMI'
+      AND domain_id != 'Measurement'
+      ) AS uci
+    ON src.unit_concept_id = uci.src_concept_id
