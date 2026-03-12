4613196acb48bb5cea6d8582c33157f8164f9d32
brendagutman <brenda.gutman@gmail.com>
2026-03-12 16:31:56 +0000

    Edit stb person

--- dbt_project/models/export/emerge_consort_gira_stb_person.sql
+++ dbt_project/models/export/emerge_consort_gira_stb_person.sql
@@ -2,7 +2,7 @@
 
     select
     emerge_id::integer as "person_id",
-    gender_concept_id::integer as "gender_concept_id",
+    s_gender_concept_id::integer as "gender_concept_id",
     year_of_birth::integer as "year_of_birth",
     '06'::integer as "month_of_birth",
     '15'::integer as "day_of_birth",
@@ -16,8 +16,7 @@
     gender_concept_id::text as "gender_source_value",
     gender_concept_id::integer as "gender_source_concept_id",
     null::text as "race_source_value",
-    s_race_concept_id::integer as "race_source_concept_id",
+    null::integer as "race_source_concept_id",
     null::text as "ethnicity_source_value",
-    ethnicity_concept_id::integer as "ethnicity_source_concept_id",
+    null::integer as "ethnicity_source_concept_id",
     from {{ ref('emerge_consort_gira_int_person_persons') }}
-   
